ba5e3d73eacaa8b78f619378cbaaf4c737b64dd2
doncan-orozco <106181181+doncan-orozco@users.noreply.github.com>
2024-02-19 05:15:16 -0600

    Feature: Add closure tree (#8)
    * Closure tree setup
    
    * Add closure tree to pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,8 @@
 # freeze_string_literal: true
 
 class Pokemon < ApplicationRecord
+  has_closure_tree
+
   has_many :pokemons_types, dependent: :destroy
   has_many :types, through: :pokemons_types
   has_one :species, dependent: :destroy
