fe80790e9f5a78ebd2e5027cb2f62d95c104102a
Doncan Orozco <doncan.orozco@gmail.com>
2024-02-19 22:23:47 -0600

    Add CreateOrUpdatePokemonWithAssociationsService and CreateEvolutionsService

---
+++ app/services/create_evolutions_service.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class CreateEvolutionsService < ApplicationService
+  def initialize(evolution_chain_response)
+    @evolution_chain_response = evolution_chain_response
+    @pokemon_chain = []
+  end
+
+  def process
+    create_pokemon_evolution_chain
+    create_closure_tree
+  end
+
+  private
+
+  attr_reader :evolution_chain_response
+  attr_accessor :pokemon_chain
+
+  def create_pokemon_evolution_chain
+    chain = evolution_chain_response["chain"]
+
+    pokemon_chain << CreateOrUpdatePokemonWithAssociationsService.for(chain["species"]["name"])
+
+    while chain["evolves_to"].present?
+      chain = chain["evolves_to"].first
+      pokemon_chain << CreateOrUpdatePokemonWithAssociationsService.for(chain["species"]["name"])
+    end
+
+    pokemon_chain
+  end
+
+  def create_closure_tree
+    pokemon_chain.each_with_index do |pokemon, index|
+      if index > 0
+        pokemon.add_child(pokemon_chain[index - 1])
+      end
+    end
+  end
+end
