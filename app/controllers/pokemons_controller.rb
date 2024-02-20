a4b63f876b36579b12f31688add0b943b8c5bcbb
Doncan Orozco <doncan.orozco@gmail.com>
2024-02-20 14:16:31 -0600

    Show evolution tree on pokemon show. Improve UI

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,10 +2,11 @@
 
 class PokemonsController < ApplicationController
   def index
-    @pagy, @pokemons = pagy(Pokemon.all.eager_load(:types, :abilities))
+    @pagy, @pokemons = pagy(Pokemon.eager_load(:types, :abilities).order(:pokedex_id))
   end
 
   def show
     @pokemon = Pokemon.eager_load(:types, :abilities, :species).find(params[:id])
+    @pokemons = @pokemon.root&.self_and_descendants&.eager_load(:types, :abilities, :species)&.reverse || Pokemon.none
   end
 end
